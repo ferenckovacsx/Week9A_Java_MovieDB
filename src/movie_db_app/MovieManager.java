57d6e65e5973d3bafbf774f2ad5bcee4de79d3f0
ferenckovacsx <ferenckovacsx@gmail.com>
2016-01-25 08:14:34 +0100

    first commit

---
+++ src/movie_db_app/MovieManager.java
@@ -0,0 +1,10 @@
+package movie_db_app;
+
+public class MovieManager {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/movie_db_app/Person.java
@@ -0,0 +1,10 @@
+package movie_db_app;
+
+public class Person {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
